13757cab1c34d58f85dc78f196375fc4db4298c1
weslyvo81 <wesly.vo@gada.io>
2022-12-14 17:33:17 +1100

    pushed id key and composite key

--- script/create_table.sql
+++ script/create_table.sql
@@ -1,9 +1,11 @@
 -- gennydb.`userstore` definition
 
 CREATE TABLE user_store (
+  id bigint NOT NULL AUTO_INCREMENT,
   realm varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   usercode varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   jti_access varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   last_active bigint,
-  PRIMARY KEY (usercode,realm)
+  PRIMARY KEY (id),
+  UNIQUE KEY userstore_uniquekey(realm,usercode)
 ) ENGINE=InnoDB AUTO_INCREMENT=7092 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
