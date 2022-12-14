f8150a257c004a603b26c4ae2e917bc1112d24e6
weslyvo81 <wesly.vo@gada.io>
2022-12-14 17:24:43 +1100

    pushed composite key

--- script/create_table.sql
+++ script/create_table.sql
@@ -3,7 +3,7 @@
 CREATE TABLE user_store (
   realm varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   usercode varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  jtiaccess varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  timestamp bigint,
+  jti_access varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  last_active bigint,
   PRIMARY KEY (usercode,realm)
 ) ENGINE=InnoDB AUTO_INCREMENT=7092 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
